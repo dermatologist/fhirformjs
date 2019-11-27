4b465db479662af1789e4c7a36959d864409df7c
Bell Eapen <github_public@gulfdoctor.net>
2019-11-26 21:26:46 -0500

    fix: tests with async await

--- test/index.test.js
+++ test/index.test.js
@@ -3,26 +3,22 @@ const fetch = require('isomorphic-fetch');
 const Fhirformjs = require('../src/index');
 
 describe('fhirformjs', () => {
-  it('should generate Fhirform', () => {
-    fetch('http://hapi.fhir.org/baseDstu3/Questionnaire/417960/_history/1?_pretty=true&_format=json')
+  it('should generate Fhirform', async () => {
+    await fetch('http://hapi.fhir.org/baseDstu3/Questionnaire/417960/_history/1?_pretty=true&_format=json')
       .then((response) => response.json())
-      .then((myJson) => {
+      .then(async (myJson) => {
         const ff = Fhirformjs.fhirformjs(myJson);
-        console.log(ff.schema);
-        console.log(ff.ui);
+        console.log(ff);
       });
+
   });
 
-  it('should add response to QuestionnaireResponse', () => {
-    fetch('http://hapi.fhir.org/baseDstu3/Questionnaire/417960/_history/1?_pretty=true&_format=json')
+  it('should add response to QuestionnaireResponse', async () => {
+    await fetch('http://hapi.fhir.org/baseDstu3/Questionnaire/417960/_history/1?_pretty=true&_format=json')
       .then((response) => response.json())
       .then((myJson) => {
         const ff = Fhirformjs.fhirformResp(myJson, {});
         console.log(ff);
-        ff.forEach(function(element) {
-          const value = element;
-          console.log(value);
-        });
       });
   });
 });
