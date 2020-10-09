25d418083d534fcc077652dbdbf48dbac6ce8006
beapen <github@gulfdoctor.net>
2020-10-09 16:29:23 +0000

    Add tests

--- test/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { FhirFormVue } from '../src/fhirformvue';
-import { Testq1 } from '../src/example/testq1'
-
-describe('blah', () => {
-  it('works', () => {
-    expect(FhirFormVue(Testq1)).toBeDefined();
-  });
-});
---
+++ test/read.test.ts
@@ -0,0 +1,8 @@
+import { Testq1 } from './example/testq1';
+
+describe('read', () => {
+  it('works', () => {
+    console.log(Testq1.id);
+    // expect(sum(1, 1)).toEqual(2);
+  });
+});
