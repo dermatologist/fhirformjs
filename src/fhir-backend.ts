9adaafb5a0fc1f5ffef0fc6486266bd7f5c8766b
Bellraj Eapen <github@gulfdoctor.net>
2021-05-06 08:03:09 -0700

    getques and toc complete

--- src/fhir-backend.ts
+++ src/fhir-backend.ts
@@ -1,10 +1,17 @@
 // Based on SMART on FHIR Example Backend Service App @ https://github.com/smart-on-fhir/sample-apps-stu3/tree/master/backend-service
 import axios from 'axios'
 import { R4 } from '@ahryman40k/ts-fhir-types';
+
+export interface FhirBackendToc {
+    fullUrl?: string;
+    id?: string;
+}
 export class FhirBackend {
 
         baseUrl: string = "http://hapi.fhir.org/baseR4"
-        questionnaireBundle: R4.IBundle | undefined
+        questionnaireBundle: R4.IBundle = {
+            resourceType: "Bundle"
+        }
 
         constructor(baseUrl: string){
             if(baseUrl != '')
@@ -13,12 +20,23 @@ export class FhirBackend {
 
         async initialize() {
 
-            this.questionnaireBundle = await axios.get(this.baseUrl + '/Questionnaire')
-
+            const response = await axios.get(this.baseUrl + '/Questionnaire')
+            this.questionnaireBundle = response.data
         }
 
-        getQuestionnaires(): R4.IBundle | undefined {
+        getQuestionnaires(): R4.IBundle {
             return this.questionnaireBundle;
+        }
+
+        getTableOfContents(){
+            return this.questionnaireBundle?.entry?.map(entry => {return {fullUrl: entry.fullUrl, id: entry.resource?.id}})
+        }
+
+        getQuestionnaire(id: string){
+            return this.questionnaireBundle?.entry?.find(entry => {
+                console.log(entry.resource?.id?.toString() + id)
+                entry.resource?.id?.toString() === id
+            })
         }
 
 
--- test/fhir-backend.test.ts
+++ test/fhir-backend.test.ts
@@ -1,16 +1,33 @@
 import { FhirBackend } from '../src/fhir-backend'
+import { R4 } from '@ahryman40k/ts-fhir-types';
 
 describe('Testing Fhir Backend', () => {
 
-
-    it('counts questionnaires on FHIR server', async () => {
-            const backend = new FhirBackend('');
-            await backend.initialize()
-            console.log(backend.getQuestionnaires())
-            //expect(ff.schema.title).toBe('f201');
+    const backend = new FhirBackend('');
+    beforeAll( async () => {
+        await backend.initialize()
     });
 
+    it('gets questionnaires on FHIR server', async () => {
 
+        if(backend.getQuestionnaires() != undefined){
+            const bundle: R4.IBundle = backend.getQuestionnaires()
+            expect(bundle.resourceType).toBe('Bundle')
+        }else{
+            throw new Error("Bundle not found");
+        }
+    });
 
+    it('gets table of contents', async () => {
 
+        const toc: unknown = backend.getTableOfContents()
+        expect(toc).toBeTruthy()
+    });
+
+    it('gets single questionnaire', async () => {
+        
+        console.log(backend.getQuestionnaire('2050148'))
+        const questionnaire: R4.IQuestionnaire = {...backend.getQuestionnaire('2050138'), resourceType: 'Questionnaire'}
+        console.log(questionnaire)
+    });
 });
